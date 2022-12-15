964997deafd8eb7c3405e2c61e86983d2c9560bd
isXander <xandersmith2008@gmail.com>
2022-12-15 18:28:51 +0000

    fix BlockMixin

--- src/main/java/dev/isxander/culllessleaves/mixins/BlockMixin.java
+++ src/main/java/dev/isxander/culllessleaves/mixins/BlockMixin.java
@@ -2,8 +2,7 @@ package dev.isxander.culllessleaves.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.isxander.culllessleaves.CullLessLeaves;
-import me.fallenbreath.conditionalmixin.api.annotation.Condition;
-import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import dev.isxander.culllessleaves.compat.Compat;
 import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -11,12 +10,11 @@ import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Restriction(conflict = { @Condition("moreculling"), @Condition("sodium") })
 @Mixin(Block.class)
 public class BlockMixin {
     @ModifyExpressionValue(method = "shouldDrawSide", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isSideInvisible(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z"))
     private static boolean shouldCullLeafSide(boolean isSideInvisible, BlockState state, BlockView world, BlockPos pos, Direction side, BlockPos blockPos) {
-        if (!(state.getBlock() instanceof LeavesBlock) || !CullLessLeaves.getConfig().enabled)
+        if (!(state.getBlock() instanceof LeavesBlock) || !CullLessLeaves.getConfig().enabled || Compat.SODIUM)
             return isSideInvisible;
 
         return CullLessLeaves.shouldCullSide(world, pos, side);
